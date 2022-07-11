f081f1b3e4bc65cc0d8bb476e43c1d93a93906b6
Sridhar <venkatasridhar.p@gmail.com>
2022-07-11 20:14:54 +0100

    Added more Test cases to cover posible scenarios

--- src/test/java/com/lendingwork/supermarket/controller/SuperMarketIntegrationTest.java
+++ src/test/java/com/lendingwork/supermarket/controller/SuperMarketIntegrationTest.java
@@ -12,16 +12,18 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-//@AutoConfigureMockMvc
 public class SuperMarketITTest {
 
     @Autowired
     WebApplicationContext wac;
     MockMvc mockMvc;
+
+    private MockRestServiceServer mockServer;
+    private ObjectMapper mapper = new ObjectMapper();
 
     @Before
     public void setup() {
@@ -30,11 +32,121 @@ public class SuperMarketITTest {
     }
 
     @Test
-    public void calculateAmountTest() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/calculate-price?items=A,B")
-        .contentType(MediaType.APPLICATION_JSON_VALUE)
-        .accept(MediaType.APPLICATION_JSON_VALUE)
-        ).andExpect(status().isOk());
+    public void calculateAmountTest_Success() throws Exception {
+
+        //When
+        MockHttpServletResponse mockResponse = mockMvc.perform(MockMvcRequestBuilders.get("/calculate-price?items=A,B")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andReturn().getResponse();
+        //then
+        assertThat(mockResponse.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(mockResponse.getContentAsString()).contains("110");
     }
+
+    @Test
+    public void calculateAmount_WhenNoOffer() throws Exception {
+
+        //When
+        MockHttpServletResponse mockResponse = mockMvc.perform(MockMvcRequestBuilders.get("/calculate-price?items=A,B,A,A,D")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andReturn().getResponse();
+        //then
+        assertThat(mockResponse.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(mockResponse.getContentAsString()).contains("360");
+    }
+
+    @Test
+    public void calculateAmount_WhenBuy2For1Offer() throws Exception {
+
+        //When
+        MockHttpServletResponse mockResponse = mockMvc.perform(MockMvcRequestBuilders.get("/calculate-price?items=B,B")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andReturn().getResponse();
+        //then
+        assertThat(mockResponse.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(mockResponse.getContentAsString()).contains("100");
+    }
+
+    @Test
+    public void calculateAmount_WhenBuy3Get1FreeOffer() throws Exception {
+
+        //When
+        MockHttpServletResponse mockResponse = mockMvc.perform(MockMvcRequestBuilders.get("/calculate-price?items=C,C,C")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andReturn().getResponse();
+        //then
+        assertThat(mockResponse.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(mockResponse.getContentAsString()).contains("75");
+    }
+
+    @Test
+    public void calculateAmount_WhenMultiBuyOffer() throws Exception {
+
+        //When
+        MockHttpServletResponse mockResponse = mockMvc.perform(MockMvcRequestBuilders.get("/calculate-price?items=D,E")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andReturn().getResponse();
+        //then
+        assertThat(mockResponse.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(mockResponse.getContentAsString()).contains("300");
+    }
+
+    @Test
+    public void calculateAmount_WhenMultiBuyOffer_NoOffer() throws Exception {
+
+        //When
+        MockHttpServletResponse mockResponse = mockMvc.perform(MockMvcRequestBuilders.get("/calculate-price?items=D,E,A,B")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andReturn().getResponse();
+        //then
+        assertThat(mockResponse.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(mockResponse.getContentAsString()).contains("410");
+    }
+
+    @Test
+    public void calculateAmount_WhenMultiBuyOffer_2for1_Offer() throws Exception {
+
+        //When
+        MockHttpServletResponse mockResponse = mockMvc.perform(MockMvcRequestBuilders.get("/calculate-price?items=D,E,B,B")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andReturn().getResponse();
+        //then
+        assertThat(mockResponse.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(mockResponse.getContentAsString()).contains("400");
+    }
+
+    @Test
+    public void calculateAmount_2for1_Offer_ItemD() throws Exception {
+
+        //When
+        MockHttpServletResponse mockResponse = mockMvc.perform(MockMvcRequestBuilders.get("/calculate-price?items=D,D,B,B")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andReturn().getResponse();
+        //then
+        assertThat(mockResponse.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(mockResponse.getContentAsString()).contains("400");
+    }
+
+    @Test
+    public void calculateAmount_All_Offers() throws Exception {
+
+        //When
+        MockHttpServletResponse mockResponse = mockMvc.perform(MockMvcRequestBuilders.get("/calculate-price?items=D,D,E,C,C,C,E,B,B")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andReturn().getResponse();
+        //then
+        assertThat(mockResponse.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(mockResponse.getContentAsString()).contains("775");
+    }
+
 
 }
