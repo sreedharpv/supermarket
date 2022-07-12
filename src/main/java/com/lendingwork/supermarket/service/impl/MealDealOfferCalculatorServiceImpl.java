7c7ed78c71db12c44bd0aadc4951467a77e5938e
Sridhar <venkatasridhar.p@gmail.com>
2022-07-12 07:30:34 +0100

    Meal Deal Offer logic added. 7 test cases passed

--- src/main/java/com/lendingwork/supermarket/service/impl/MealDealOfferCalculatorServiceImpl.java
+++ src/main/java/com/lendingwork/supermarket/service/impl/MealDealOfferCalculatorServiceImpl.java
@@ -9,9 +9,71 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * This class process the Meal Deal offer
+ */
 @Service
 public class MealDealOfferCalculatorServiceImpl implements MealDealOfferCalculatorService {
     public void priceOfferCalculator(ItemInfo itemInfo, List<ItemPriceProcessor> itemPriceProcessorLst) {
+        //Buy D and E for £3
+        String offerForItem = itemInfo.getOffer();
+        String[] offerInfoArr = offerForItem.split("\\s|£");
+        String offerItems1 = offerInfoArr[1];
+        String offerItems2 = offerInfoArr[3];
+        int offerPrice = Integer.parseInt(offerInfoArr[6]);
 
+        List<ItemPriceProcessor> unProcessedItemPriceProcessorLst =
+                getOfferUnProcessedItems(offerItems1, offerItems2, itemPriceProcessorLst);
+
+        if (unProcessedItemPriceProcessorLst.isEmpty()) {
+            ItemPriceProcessor itemPriceProcessor = new ItemPriceProcessor();
+            itemPriceProcessor.setItem(itemInfo.getItem());
+            itemPriceProcessor.setUnitPrice(itemInfo.getUnitPrice());
+            itemPriceProcessor.setItemTotalPrice(itemInfo.getUnitPrice());
+            itemPriceProcessorLst.add(itemPriceProcessor);
+        } else {
+            boolean mealDealMatchFound = false;
+            for(ItemPriceProcessor itemPriceProcessor : unProcessedItemPriceProcessorLst) {
+                if(!itemInfo.getItem().equalsIgnoreCase(itemPriceProcessor.getItem())) {
+                    mealDealMatchFound = true;
+                    itemPriceProcessor.setItemTotalPrice(0);
+                    itemPriceProcessor.setOfferProcessed(true);
+                    //add given item
+                    ItemPriceProcessor givenItemPriceProcessor = new ItemPriceProcessor();
+                    givenItemPriceProcessor.setItem(itemInfo.getItem());
+                    givenItemPriceProcessor.setUnitPrice(itemInfo.getUnitPrice());
+                    givenItemPriceProcessor.setItemTotalPrice(offerPrice*100);
+                    givenItemPriceProcessor.setOfferProcessed(true);
+                    unProcessedItemPriceProcessorLst.add(givenItemPriceProcessor);
+                    break;
+                }
+            }
+
+            if (!mealDealMatchFound) {
+                ItemPriceProcessor itemPriceProcessor = new ItemPriceProcessor();
+                itemPriceProcessor.setItem(itemInfo.getItem());
+                itemPriceProcessor.setUnitPrice(itemInfo.getUnitPrice());
+                itemPriceProcessor.setItemTotalPrice(itemInfo.getUnitPrice());
+                itemPriceProcessor.setOfferProcessed(false);
+                unProcessedItemPriceProcessorLst.add(itemPriceProcessor);
+            }
+        }
+        itemPriceProcessorLst.addAll(unProcessedItemPriceProcessorLst);
+    }
+
+    private List<ItemPriceProcessor> getOfferUnProcessedItems(String offerItems1, String offerItems2, List<ItemPriceProcessor> itemPriceProcessorLst) {
+        List<ItemPriceProcessor> unProcessedItemPriceProcessorLst = new ArrayList<>();
+        Iterator<ItemPriceProcessor> iterator = itemPriceProcessorLst.iterator();
+        while (iterator.hasNext()) {
+            ItemPriceProcessor itemPriceProcessor = iterator.next();
+            if ((itemPriceProcessor.getItem().equalsIgnoreCase(offerItems1) ||
+                    itemPriceProcessor.getItem().equalsIgnoreCase(offerItems1))
+                    && !itemPriceProcessor.getOfferProcessed()) {
+                unProcessedItemPriceProcessorLst.add(itemPriceProcessor);
+                iterator.remove();
+            }
+        }
+        return unProcessedItemPriceProcessorLst;
     }
 }
+
